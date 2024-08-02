f5e533e012245eada88c776150e3ca3f31285d46
Laura Oliveira <laura.oliveira@iteris.com.br>
2024-08-02 15:37:00 -0300

    Renaming business unit

--- wd-fox-dev/terraform/s3.tf
+++ wd-fox-dev/terraform/s3.tf
@@ -1,6 +1,6 @@
 data "aws_caller_identity" "current" {}
 
 resource "aws_s3_bucket" "pixar_bucket" {
-  bucket = "aft-wd-pixar-${data.aws_caller_identity.current.account_id}"
+  bucket = "aft-wd-pixar-dev-${data.aws_caller_identity.current.account_id}"
   acl    = "private"
 }
